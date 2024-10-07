4584b2fc11737a9331f358bf4c5f75aa88e99a9a
Naveen kumar <117574767+naveenkumar890@users.noreply.github.com>
2024-10-07 17:26:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Formtext from './components/Formtext';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className="container">
+    <Formtext heading='React-friendly JSX code effortlessly'/>
+    </div>
+    </>
+  );
+}
+
+export default App;
